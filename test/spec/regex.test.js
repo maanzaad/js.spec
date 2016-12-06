ba4424a2736321c8f935bb437d682a31d7f0daa5
Michiel Vermeir <das.antithesis@gmail.com>
2016-12-06 17:25:34 +0100

    Fine tuning of Regex predicate and more tests.

--- test/spec/regex.test.js
+++ test/spec/regex.test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import regex from '../../lib/spec/regex'
 import { invalid } from '../../lib/symbols'
+import { explainData } from '../../lib/util'
 
 describe("regex", () => {
     describe("conform", () => {
@@ -18,6 +19,34 @@ describe("regex", () => {
                     expect(re.conform(letter)).to.equal(invalid)
                 })
             })
+        })
+
+        it("works with bad inputs", () => {
+            const re = regex(/a-z/)
+            expect(re.conform(null)).to.equal(invalid)
+            expect(re.conform(undefined)).to.equal(invalid)
+            expect(re.conform(1)).to.equal(invalid)
+            expect(re.conform([])).to.equal(invalid)
+            expect(re.conform({})).to.equal(invalid)
+            expect(re.conform("🔨")).to.equal(invalid)
+            expect(re.conform(Symbol('a'))).to.equal(invalid)            
+        })
+    })
+
+    describe("explain", () => {
+        it("outputs expected data", () => {
+            const re = regex(/a-z/)
+            const problems = explainData(re, 'A')
+
+            expect(problems).to.be.an("array").and.have.length(1)
+            expect(problems).to.have.deep.property("[0].via")
+                .that.deep.equals(["Regex(/a-z/)"])
+            expect(problems).to.have.deep.property("[0].path")
+                .that.deep.equals([])
+            expect(problems).to.have.deep.property("[0].predicate")
+                .that.is.a("function")
+            expect(problems).to.have.deep.property("[0].value")
+                .that.deep.equals("A")
         })
     })
 })
